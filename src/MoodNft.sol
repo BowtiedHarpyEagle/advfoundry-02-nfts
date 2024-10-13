fbce58e03e6d79e9127e89f2c28c5f07ecc93053
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-10-13 12:13:16 -0500

    14 7:00

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,19 +2,27 @@
 pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_happySvg;
-    string private s_sadSvg;
+    string private s_happySvgImageUri;
+    string private s_sadSvgImageUri;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping (uint256 => Mood) s_TokenIdToMood;
     
     constructor() ERC721("MoodNft", "MN") {
-        string memory sadSvg;
-        string memory happySvg;
+        string memory sadSvgImageUri;
+        string memory happySvgImageUri;
 
         s_tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
     }
         
     function mintNft() public {
@@ -22,5 +30,7 @@ contract MoodNft is ERC721 {
         s_tokenCounter++;
     }
     
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {}
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        string memory tokenMetaData = stringconcat('{"name": "' name(), '", "description": "An NFT that reflects owners mood " , "attributes"2 : [{"trait_type": "moodiness", "value": 100}], "image": "', imageURI,'"}'); 
+    }
 }
